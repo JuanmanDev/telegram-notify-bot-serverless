61fbcfb911416b96a8c3c766b869636ff61ad7bb
Juan Manuel Béc <Juanmabs22@outlook.com>
2020-12-05 16:08:37 +0100

    Added telegraf dynamodb middleware and keyboard. Working keyboard example with Image

--- bot.js
+++ bot.js
@@ -1,8 +1,20 @@
 const { Telegraf } = require('telegraf');
+const DynamoDBSession = require('telegraf-session-dynamodb')
+
+const { Keyboard } = require('telegram-keyboard')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const dynamoDBSession = new DynamoDBSession({
+    dynamoDBConfig: {
+        params: {
+            TableName: "TakeoffSpaceXDynamoDbTable"
+        },
+        region: process.env.AWS_REGION
+    }
+})
 
-const { createChat } = require('./chats');
+
+bot.use(dynamoDBSession.middleware())
 
 const helpMsg = `The bot just repeats anything you say in the chat.
 \n*Command reference:*
@@ -12,7 +24,6 @@ const helpMsg = `The bot just repeats anything you say in the chat.
     /help - Show this help page`;
 
 bot.start((ctx) => {
-    createChat(ctx.chat.id);
     return ctx.reply(`Hello from Lambda, ${ctx.from.first_name ? ctx.from.first_name : 'friend'}! Use /help to view available commands.`);
 });
 
@@ -22,17 +33,31 @@ bot.help((ctx) => {
 
 bot.command('whoami', (ctx) => {
     let userInfo = JSON.stringify(ctx.from);
-    return ctx.reply(`User info: ${userInfo}`);
-})
+    let session = JSON.stringify(ctx.session);
+    return ctx.reply(`User info: ${userInfo} , Session: ${session}`);
+});
 
 bot.command('ping', (ctx) => {
     return ctx.replyWithMarkdown('*Pong!*');
-})
+});
+
+bot.command('guess', async ({ reply, replyWithPhoto }) => {
+    const keyboard = Keyboard.make([
+      ['Button 1', 'Button 2'], // First row
+      ['Button 3', 'Button 4'], // Second row
+    ])
+  
+    
+    await replyWithPhoto('https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/1234/', {
+        ...keyboard.reply(), 
+        ...keyboard.inline(),
+    });
+  })
 
 bot.on('text', (ctx) => {
     return ctx.reply(ctx.message.text);
-})
+});
 
 module.exports = {
     bot
-}
+};
