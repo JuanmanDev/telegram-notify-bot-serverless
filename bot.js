bf5e7c9707de299ec75c35c5a6ff07631c3ce69d
Juan Manuel Béc <Juanmabs22@outlook.com>
2020-12-05 20:01:58 +0100

    First working version

--- bot.js
+++ bot.js
@@ -6,14 +6,20 @@ const { CallbackData } = require('telegraf-callback-data');
 const { Keyboard, Key } = require('telegram-keyboard')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 const dynamoDBSession = new DynamoDBSession({
     dynamoDBConfig: {
         params: {
-            TableName: "TakeoffSpaceXDynamoDbTable"
+            TableName: process.env.DYNAMODB_TABLE
         },
         region: process.env.AWS_REGION
-    }
-})
+    },
+});
+bot.use(dynamoDBSession.middleware());
+
+const FIRST_FRAME = 0;
+const LAST_FRAME = 61696;
+const URL_FRAMES = 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/';
 
 const CALLBACK_TYPE_BEFORE = 'before';
 const CALLBACK_TYPE_SELECT = 'this';
@@ -23,13 +29,14 @@ const CALLBACK_TYPE_EXIT   = 'exit';
 const framesCD = new CallbackData('frames', ['type']);
 
 
-bot.use(dynamoDBSession.middleware())
 
 const helpMsg = `The bot just repeats anything you say in the chat.
 \n*Command reference:*
     /start - Start bot
+    /guess - Start the guess the take off from SpaceX
     /ping - *Pong!*
     /whoami - Show information about the current user
+    /clean - Removes all user data
     /help - Show this help page`;
 
 bot.start((ctx) => {
@@ -50,7 +57,39 @@ bot.command('ping', (ctx) => {
     return ctx.replyWithMarkdown('*Pong!*');
 });
 
-bot.command('guess', async ({ reply, replyWithPhoto }) => {
+bot.command('clean', (ctx) => {
+    ctx.session = null;
+    return ctx.replyWithMarkdown('*Pong!* - All user data cleared');
+});
+
+
+
+bot.command('guess', async (ctx) => {
+    const { reply, replyWithPhoto, session } = ctx;
+
+    await reply(`Welcome to guess the takeoff, every round you will receive a image with a SpaceX takeoff, and you have to reply telling me if the takoff is:
+    ⏪ Before the image
+    ☑ Inside the image
+    ⏩ After the image`);
+
+    ctx.session.firstFrame = FIRST_FRAME;
+    ctx.session.lastFrame = LAST_FRAME;
+    
+    await askForFrame(ctx);
+});
+
+async function askForFrame({ reply, replyWithPhoto, session }) {
+    
+    session.currentFrame = Math.trunc((session.lastFrame - session.firstFrame) / 2) + session.firstFrame;
+  
+    if (session.firstFrame >= session.lastFrame 
+        || session.firstFrame > session.currentFrame 
+        || session.lastFrame < session.currentFrame 
+        ) {
+        reply("You finish, your frame is the: " + session.currentFrame);
+        replyWithPhoto(URL_FRAMES + session.currentFrame);
+        return;
+    }
     const keyboard = Keyboard.make([
         [
             Key.callback(' ⏪ ',  framesCD.new({ type: CALLBACK_TYPE_BEFORE })),
@@ -58,27 +97,30 @@ bot.command('guess', async ({ reply, replyWithPhoto }) => {
             Key.callback(' ⏩ ',  framesCD.new({ type: CALLBACK_TYPE_AFTER  })),
         ],
         [
-            Key.callback('Exit', 'exit'), // TODO EXIT
+            Key.callback(' EXIT ',  framesCD.new({ type: CALLBACK_TYPE_EXIT  })),
         ],
       ]);
-  
+      
     
-    await replyWithPhoto('https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/1234/', {
+    await replyWithPhoto(URL_FRAMES + session.currentFrame, {
         ...keyboard.reply(), 
         ...keyboard.inline(),
     });
+}
+
+bot.action(framesCD.filter({ type: CALLBACK_TYPE_BEFORE }), async (ctx) => {
+    ctx.session.lastFrame = ctx.session.currentFrame;
+    await askForFrame(ctx);
 });
 
-bot.action(framesCD.filter({ type: CALLBACK_TYPE_BEFORE }), ({ answerCbQuery }) => {
-    return answerCbQuery('Hello ' + CALLBACK_TYPE_BEFORE);
+bot.action(framesCD.filter({ type: CALLBACK_TYPE_SELECT }), async (ctx) => {
+    ctx.session.currentFrame = ctx.session.lastFrame;
+    await askForFrame(ctx);
 });
 
-bot.action(framesCD.filter({ type: CALLBACK_TYPE_SELECT }), ({ answerCbQuery }) => {
-    return answerCbQuery('Hello ' + CALLBACK_TYPE_SELECT);
-});
-
-bot.action(framesCD.filter({ type: CALLBACK_TYPE_AFTER }), ({ answerCbQuery }) => {
-    return answerCbQuery('Hello ' + CALLBACK_TYPE_AFTER);
+bot.action(framesCD.filter({ type: CALLBACK_TYPE_AFTER }), async (ctx) => {
+    ctx.session.firstFrame = ctx.session.currentFrame + 1;
+    await askForFrame(ctx);
 });
 
 bot.action(framesCD.filter({ type: CALLBACK_TYPE_EXIT }), ({ answerCbQuery }) => {
